980eaf4198bb02dcda3a818d8452abad7f8e4f7b
tharun <tharun6934@gmail>
2024-07-08 08:27:41 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import places from "./Place.json";
+import { useState } from "react";
 
 function App() {
+  const [search, setSearch] = useState("");
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen p-10 ">
+      <center>
+        <div>
+          <p className="text-7xl font-semibold mb-10">Destination Search</p>
+          <input
+            className="bg-slate-300 rounded-xl text-black text-3xl p-4 mb-10"
+            placeholder="Search "
+            type="text"
+            m
+            value={search}
+            onChange={handleChange}
+          />
+        </div>
+      </center>
+      <div className="flex  flex-wrap">
+        {places
+          .filter((place) => {
+            return place.title
+              .toLocaleLowerCase()
+              .includes(search.toLocaleLowerCase());
+          })
+          .map((place) => {
+            return (
+              <div className="w-1/3 p-4">
+                <img src={place.image} className="rounded-2xl mb-5" />
+                <p className="text-3xl mb-5">{place.title}</p>
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 }
